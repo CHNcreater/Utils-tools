f191fbffa0a92330d08bb71006628b3a273e8129
qiaoyinbo <breakerjoe@163.com>
2025-06-19 21:36:20 +0800

    init sidebar

--- app/[lang]/Tools/layout.tsx
+++ app/[lang]/Tools/layout.tsx
@@ -1,12 +1,13 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "../components/app-sidebar";
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      {children}
-    </div>
+      <SidebarProvider defaultOpen={true}>
+        <AppSidebar />
+        <main>
+          {children}
+        </main>
+      </SidebarProvider>
   );
 }
