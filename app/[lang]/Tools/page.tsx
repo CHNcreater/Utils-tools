8384ffd125bd575ac10c8d19a1737beeb73df37d
qiaoyinbo <breakerjoe@163.com>
2025-06-26 22:08:02 +0800

    add tool page and i18n tool data

--- app/[lang]/Tools/page.tsx
+++ app/[lang]/Tools/page.tsx
@@ -1,17 +1,21 @@
 import { getDictionary } from "@/get-dictionary";
 import { Locale } from "@/i18n-config";
+import { ToolCard } from "../components/tool-card";
 
 export default async function Tools({
-  params
-} : {
+  params,
+}: {
   params: Promise<{ lang: Locale }>;
 }) {
-  const {lang} = await params;
+  const { lang } = await params;
   const dictionary = await getDictionary(lang);
 
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex flex-col items-center h-full">
       <h1 className="text-2xl font-bold">{dictionary.tools.title}</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-6xl p-4">
+        <ToolCard />
+      </div>
     </div>
   );
 }
---
+++ app/[lang]/components/tool-card.tsx
@@ -0,0 +1,28 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export function ToolCard() {
+  return (
+    <Card className="w-full h-full h-50 w-50 bg-gradient-to-br from-gray-50 via-gray-100 to-gray-200 dark:from-gray-800 dark:via-gray-900 dark:to-gray-950 shadow-md hover:shadow-lg transition-shadow duration-300 dark:shadow-[0_4px_24px_rgba(0,0,0,0.8)]">
+      <CardHeader>
+        <CardTitle>GUID/UUID Generator</CardTitle>
+        <CardDescription>World</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1"></CardContent>
+      <CardFooter>
+        <CardAction>
+          <a href='/' target="_blank" rel="noopener noreferrer">
+            Open Tool
+          </a>
+        </CardAction>
+      </CardFooter>
+    </Card>
+  );
+}
